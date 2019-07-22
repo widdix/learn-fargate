1cf6484eeccfdda20f426644bddae748cf6ba085
Michael Wittig <michael@widdix.de>
2019-07-22 09:54:15 +0200

    update templates

--- templates/update.sh
+++ templates/update.sh
@@ -6,6 +6,3 @@ rm cluster.yaml && wget https://s3-eu-west-1.amazonaws.com/widdix-aws-cf-templat
 rm vpc-2azs.yaml && wget https://s3-eu-west-1.amazonaws.com/widdix-aws-cf-templates-releases-eu-west-1/stable/vpc/vpc-2azs.yaml
 
 # Check the diffs, make sure you are not overriding customizations
-
-
-
